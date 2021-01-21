93339d5b73109baf8f5cfd43fdc9d4243beeb754
KumaTheGreat <kuma3kun@gmail.com>
2021-01-22 02:11:06 +0900

    スライドインアニメーション試作

--- Assets/MesgakiManagement/UI/CustomerQueue.cs
+++ Assets/MesgakiManagement/UI/CustomerQueue.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CustomerQueue : MonoBehaviour
 {
@@ -9,29 +10,41 @@ public class CustomerQueue : MonoBehaviour
     // Imageプレハブ
     [SerializeField] GameObject customerQueueImage;
 
+    // Image配置枠のプレハブ
+    [SerializeField] GameObject customerQueueImageSlot;
+
+    // 親オブジェクトの指定
+    [SerializeField] Transform queueParent;
+
     // 親オブジェクトの指定
     [SerializeField] Transform characterParent;
 
     // 基準点用
-    [SerializeField] GameObject firstCustomerImage;
+    [SerializeField] Image firstCustomerImage;
 
-    private int currentMax = 6;
+    // 現在のキュー内の客アイコン数
+    private int currentQueueMax = 0;
+
+    GameObject queueImageInstance;
 
     private float firstX;
     private float firstY;
     private float firstZ;
 
+
+
+    GameObject slotInstance;
+    GameObject childGameObject;
+    RectTransform childTransform;
+
     // Start is called before the first frame update
     void Start()
     {
-        //GameObject obj = (GameObject)Resources.Load("CustomerQueueImage");
         Debug.Log("プレハブ存在チェック：" + customerQueueImage);
 
-        Vector3 first = firstCustomerImage.transform.position;
-
-        Debug.Log("初期位置ポジション：" + firstCustomerImage.transform.position);
-
-        Debug.Log("初期位置ポジション：" + firstCustomerImage.transform.localPosition);
+        //Vector3 first = firstCustomerImage.rectTransform.position;
+        RectTransform slot = customerQueueImageSlot.GetComponent<RectTransform>();
+        Vector3 first = slot.position;
 
         // 初期座標取_得
         firstX = first.x;
@@ -39,25 +52,36 @@ public class CustomerQueue : MonoBehaviour
         firstZ = first.z;
 
 
-
-        // プレハブを元にオブジェクトを生成する
-        GameObject queueImageInstance = (GameObject)Instantiate(
-            customerQueueImage
-            , new Vector3(firstX, firstY, firstZ)
-            //, firstCustomerImage.transform.position
-            , Quaternion.identity
-            , characterParent);
-        queueImageInstance.name = "CustomerQueueImage" + (currentMax + 1).ToString();
+        addQueue();
+        
     }
+
+    private float MoveTimer;
 
     // Update is called once per frame
     void Update()
     {
-        
+        var sumTime = 0f;
+        var flag = true;
+
+        MoveTimer += Time.deltaTime;
+
+        Debug.Log("時間測定：" + MoveTimer);
+
+        childTransform.localPosition = Vector3.Lerp(new Vector2(300, 0), new Vector2(0, 0), MoveTimer * 2);
     }
 
     void addQueue()
     {
-        //customerQueue
+        // プレハブを元にオブジェクトを生成する
+        slotInstance = (GameObject)Instantiate(
+            customerQueueImageSlot
+            , new Vector3(firstX, firstY, firstZ)
+            , Quaternion.identity
+            , queueParent);
+        childGameObject = slotInstance.transform.GetChild(0).gameObject;
+        childTransform = childGameObject.GetComponent<RectTransform>();
+
+        slotInstance.name = "CustomerQueueImageSlot" + (currentQueueMax + 1).ToString();
     }
 }
